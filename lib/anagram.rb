12c2d021a28301aab52f1a10659408b11a2f4761
Travis Exline <t.exline22@gmail.com>
2019-05-23 13:21:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,13 @@ class Anagram
     @word = word
   end
 
-  def match(array)
+  def match(words)
+    anagram_array = []
+    words.each do |new_anagram|
+      if new_anagram.split(" ").sort == words.split(" ").sort
+        anagram_array << new_anagram
+      end
+    end
+    anagram_array
   end
 end
