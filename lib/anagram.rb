741e4ce0a6778c773b3c801fbd97fd5a02327ef4
Travis Exline <t.exline22@gmail.com>
2019-05-23 13:34:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,12 +7,6 @@ class Anagram
   end
 
   def match(array)
-    match_array = []
-    array.each do |new_anagram|
-      if new_anagram.split(//).sort == @name.split(//).sort
-        new_anagram << match_array
-      end
-    end
-    match_array
+    array.select {|new_anagram| new_anagram.split(", ").sort == @name.split(", ").sort}
   end
 end
